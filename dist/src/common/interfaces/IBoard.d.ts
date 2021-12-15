83ed9b5b54b7fb53bfd70804f095e006e912fa93
Rotem Grimberg <rotemgrim@gmail.com>
2021-12-15 12:21:20 +0200

    add liveboard interface

---
+++ dist/src/common/interfaces/IBoard.d.ts
@@ -0,0 +1,38 @@
+import { IOldGeneral } from "./oldBoardTypes/IOldGeneral";
+import { SectionConfig } from "./ISection";
+import { WidgetConfig } from "./IWidget";
+import { LitElement } from "lit";
+import { LiveWidget } from "../LiveWidget";
+import { FloatPos, GridPos } from "./IPositions";
+export declare type BoardConfig = {
+    boardId: string;
+    meta: IOldGeneral;
+    grid: {
+        maxWidth: string;
+        gap: {
+            x: string;
+            y: string;
+        };
+        columns: {
+            colNum: number;
+            colWidth: string;
+        };
+        rows: {
+            rowNum: number;
+            rowHeight: string;
+        };
+    };
+    sections: SectionConfig[];
+    widgets: WidgetConfig[];
+};
+export interface ILiveBoard extends LitElement {
+    exampleConfig: BoardConfig;
+    config: BoardConfig;
+    widgetsEl: LiveWidget[];
+    getGridStyling(): string;
+    positionToGridArea(p: GridPos): string;
+    getWidgetPos(p: GridPos | FloatPos): string;
+    setRows(n: number): void;
+    preRender(): void;
+    addScriptForWidget(w: WidgetConfig): Promise<WidgetConfig>;
+}
