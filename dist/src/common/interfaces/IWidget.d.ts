38e3e200b6ea49f82e01764a8264ae2b2e6d7c77
Rotem Grimberg <rotemgrim@gmail.com>
2021-12-16 20:31:33 +0200

    add section number into widget config

--- dist/src/common/interfaces/IWidget.d.ts
+++ dist/src/common/interfaces/IWidget.d.ts
@@ -1,6 +1,7 @@
 import { FloatPos, GridPos } from "./IPositions";
 export declare type WidgetConfig = {
     id: string;
+    section: number;
     position: GridPos | FloatPos;
     widgetTag: string;
     widgetScripts: string;
--- src/common/interfaces/IWidget.ts
+++ src/common/interfaces/IWidget.ts
@@ -3,6 +3,7 @@ import {FloatPos, GridPos} from "./IPositions";
 
 export type WidgetConfig = {
     id: string;
+    section: number;
     position: GridPos | FloatPos;
     widgetTag: string; // 'folloze-header-hor' | 'header' | 'gallery' | 'banner' ...
     widgetScripts: string; // url to the widget CDN | lib
