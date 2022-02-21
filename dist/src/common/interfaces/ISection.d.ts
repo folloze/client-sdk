f399f019666123783b46233e8165dda0dd429296
Rotem Grimberg <rotemgrim@gmail.com>
2022-02-21 11:21:33 +0200

    add BackgroundImage type to ribbon config

--- dist/src/common/interfaces/ISection.d.ts
+++ dist/src/common/interfaces/ISection.d.ts
@@ -2,9 +2,15 @@ import { GridPos } from "./IPositions";
 export declare type SectionConfig = {
     id: string;
 };
+export declare type BackgroundImage = {
+    image: string;
+    position: string;
+    size: "cover" | "contain" | string;
+    repeat: string;
+};
 export declare type RibbonConfig = {
     id: string;
     sectionId: string;
     position: GridPos;
-    background: string;
+    background: string | BackgroundImage | any;
 };
--- src/common/interfaces/ISection.ts
+++ src/common/interfaces/ISection.ts
@@ -4,9 +4,16 @@ export type SectionConfig = {
     id: string;
 }
 
+export type BackgroundImage = {
+    image: string,
+    position: string,
+    size: "cover" | "contain" | string,
+    repeat: string
+}
+
 export type RibbonConfig = {
     id: string;
     sectionId: string;
     position: GridPos;
-    background: string; // url(/images/GreenBtn.svg) 50% 50% no-repeat rgb(255, 0, 0);
+    background: string | BackgroundImage | any; // url(/images/GreenBtn.svg) 50% 50% no-repeat rgb(255, 0, 0);
 }
