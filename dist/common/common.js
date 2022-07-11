7628fa48a193f6d7e9ba5dc3f04255882e049f6c
Inbal Miller <inbal@folloze.com>
2022-07-11 17:07:30 +0300

    build

--- dist/common/common.js
+++ dist/common/common.js
@@ -22,8 +22,8 @@ import {
 } from "../chunks/chunk.TBQ6SAV3.js";
 import {
   FetchService
-} from "../chunks/chunk.BTK3NVPD.js";
-import "../chunks/chunk.F7UMJQCK.js";
+} from "../chunks/chunk.J3OY5UAX.js";
+import "../chunks/chunk.62TMOTOU.js";
 import "../chunks/chunk.Y7N5PQ2A.js";
 import "../chunks/chunk.AQ3ETNLA.js";
 import "../chunks/chunk.H64FWCCZ.js";
