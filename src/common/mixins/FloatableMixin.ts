7db6b79d27ce4ca5cdafdfb21b427940e3a617d1
rotem <rotem@folloze.com>
2022-07-18 14:06:43 +0300

    fix floatable position

--- src/common/mixins/FloatableMixin.ts
+++ src/common/mixins/FloatableMixin.ts
@@ -26,8 +26,8 @@ export const Floatable = <T extends Constructor<LitElement>>(superClass: T) => {
                     //opacity: 0;
                     transition: opacity 500ms ease-in;
                     position: absolute;
-                    top: 100px;
-                    left: 150px;
+                    //top: 100px;
+                    //left: 150px;
                     z-index: 104;
                     box-shadow: var(--floatBoxShadow);
 
