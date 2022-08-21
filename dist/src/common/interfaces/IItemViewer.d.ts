a2999514c7d992ec0409f70439936e07c6ece8aa
Inbal Miller <inbal@folloze.com>
2022-08-21 09:48:14 +0300

    updated item viewer interface

--- dist/src/common/interfaces/IItemViewer.d.ts
+++ dist/src/common/interfaces/IItemViewer.d.ts
@@ -2,7 +2,7 @@ import { LitElement } from "lit";
 import { OpenItemViewerPayload } from "../../liveboard/ILiveboardTypes";
 import { LiveWidget } from "../LiveWidget";
 export interface IItemViewer extends LitElement {
-    open(item?: OpenItemViewerPayload, headerElement?: LiveWidget): void;
+    open(item?: OpenItemViewerPayload, headerElement?: LiveWidget, isLanding?: Boolean): void;
     close(): void;
     changeItem(item: OpenItemViewerPayload): any;
     stateChanged(state: any): any;
