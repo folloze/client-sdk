555fcb2f741574d49d92c719041aa0cef3f56df5
Tanya Laushkin <tanya@folloze.com>
2022-08-30 18:17:04 +0300

    fix function def

--- dist/src/common/CookieConsentWidget.d.ts
+++ dist/src/common/CookieConsentWidget.d.ts
@@ -2,5 +2,5 @@ import { LiveWidget } from "./LiveWidget";
 export declare abstract class CookieConsentWidget extends LiveWidget {
     isCookieConsentable: boolean;
     abstract onlyRegulatedCountries: boolean;
-    abstract show: () => {};
+    abstract show(): any;
 }
