3996719ba185c3be6fb9d7a1c7bc60cc1f9e1f9d
itamargolan <itamarg@folloze.com>
2022-09-04 17:56:07 +0300

    Change binding solution and add tests

--- dist/chunks/mocks.QBNHTAQ5.js
+++ dist/chunks/mocks.QBNHTAQ5.js
@@ -12,6 +12,9 @@ var rules = (mock) => {
     return [200, "ping sent!"];
   });
   mock.onPost("/live_board/v1/session_validations").reply(200);
+  mock.onPost("/live_board/v1/sessions").reply((config) => {
+    return [200, { guid: "foo" }, { "folloze-session-guid": "foo" }];
+  });
   mock.onPut("/live_board/v2/invitation_wrappers/1").reply(200);
 };
 export {
