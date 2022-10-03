c3a521775543014d078130a6c303befe30df9af0
rotem <rotem@folloze.com>
2022-10-03 14:35:38 +0300

    clean dist

--- dist/src/common/helpers/PositionHelpers.d.ts
+++
@@ -1,3 +0,0 @@
-import { FloatingWidgetConfig } from "../interfaces/IWidget";
-export declare function floatingPosStrToPercent(str: any): any;
-export declare function getFloatingWidgetPosition(fwc: FloatingWidgetConfig): string;
--- dist/src/common/interfaces/IItemViewer.d.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement } from "lit";
-import { OpenItemViewerPayload } from "../../liveboard/ILiveboardTypes";
-import { LiveWidget } from "../LiveWidget";
-export interface IItemViewer extends LitElement {
-    open(item?: OpenItemViewerPayload, headerElement?: LiveWidget, isLanding?: Boolean): void;
-    close(): void;
-    changeItem(item: OpenItemViewerPayload): any;
-    stateChanged(state: any): any;
-}
