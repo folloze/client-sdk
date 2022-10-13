e4a8bbc1e01f0d7b9e4d1562a3b7d864faf990cb
rotem <rotem@folloze.com>
2022-10-13 11:58:22 +0300

    add incoming events to item-viewer

--- dist/src/common/interfaces/IItemViewer.d.ts
+++ dist/src/common/interfaces/IItemViewer.d.ts
@@ -1,9 +1,11 @@
 import { LitElement } from "lit";
 import { OpenItemViewerPayload } from "../../liveboard/ILiveboardTypes";
 import { LiveWidget } from "../LiveWidget";
+import { FlzEvent } from "../FlzEvent";
 export interface IItemViewer extends LitElement {
     open(item?: OpenItemViewerPayload, headerElement?: LiveWidget, isLanding?: Boolean): void;
     close(): void;
     changeItem(item: OpenItemViewerPayload): any;
     stateChanged(state: any): any;
+    incomingEvents(e: FlzEvent): any;
 }
--- src/common/interfaces/IItemViewer.ts
+++ src/common/interfaces/IItemViewer.ts
@@ -1,10 +1,12 @@
-import { LitElement } from "lit";
+import {LitElement} from "lit";
 import {OpenItemViewerPayload} from "../../liveboard/ILiveboardTypes";
-import { LiveWidget } from "../LiveWidget";
+import {LiveWidget} from "../LiveWidget";
+import {FlzEvent} from "../FlzEvent";
 
 export interface IItemViewer extends LitElement {
     open(item?: OpenItemViewerPayload, headerElement?: LiveWidget, isLanding?: Boolean): void;
     close(): void;
     changeItem(item: OpenItemViewerPayload);
     stateChanged(state: any);
+    incomingEvents(e: FlzEvent);
 }
