dd269040e4e2a701f9fa48dadab8723c4b6b753c
Itamar Zilber <a>
2022-12-05 11:40:35 +0200

    restored commented platform

--- dist/analytics/Analytics.js
+++ dist/analytics/Analytics.js
@@ -1,1 +1,1 @@
-import{a,b,c,d}from"../chunks/chunk.FSQY3NNQ.js";import"../chunks/chunk.KVVA2TM3.js";export{d as Analytics,c as DesignerEventTypes,a as EventSources,b as LiveBoardEventTypes};
+import{a,b,c,d}from"../chunks/chunk.5O6JAZB6.js";import"../chunks/chunk.KVVA2TM3.js";export{d as Analytics,c as DesignerEventTypes,a as EventSources,b as LiveBoardEventTypes};
