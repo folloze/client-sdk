0a54d3288130a2a0690713b9058765ee9c6240ff
Itamar Zilber <a>
2022-12-12 12:18:36 +0200

    resolved conflicts with master

---
+++ dist/analytics/Analytics.js
@@ -0,0 +1,1 @@
+import{a,b,c,d}from"../chunks/chunk.AER5KATE.js";import"../chunks/chunk.KVVA2TM3.js";export{d as Analytics,c as DesignerEventTypes,a as EventSources,b as LiveBoardEventTypes};
---
+++ dist/chunks/chunk.TWUQ6LLZ.js
@@ -0,0 +1,1 @@
+var t=(e=>(e[e.footer=1]="footer",e[e.privacy_message=2]="privacy_message",e[e.form_privacy_message=3]="form_privacy_message",e))(t||{});export{t as a};
