db3573c2bb70a3756cb27e887ed0db801cb621e3
Dana Turjeman <dana@folloze.com>
2023-03-05 15:25:33 +0200

    new dist files

--- dist/analytics/Analytics.js
+++ dist/analytics/Analytics.js
@@ -1,1 +1,1 @@
-import{a,b,c}from"../chunks/chunk.QVZBT34S.js";import"../chunks/chunk.KVVA2TM3.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
+import{a,b,c}from"../chunks/chunk.6USUV4SD.js";import"../chunks/chunk.V2USFPSF.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
