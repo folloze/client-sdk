cd59eb42f158793fb3dbac335c4007d04b2a4299
Dana Turjeman <dana@folloze.com>
2023-03-30 13:37:55 +0300

    update dist files - pull origin master

--- dist/src/common/ContentWidget.d.ts
+++
@@ -1,5 +0,0 @@
-import { LiveWidget } from "./LiveWidget";
-import { ContentWidgetConfig } from "./interfaces/IContentWidget";
-export declare abstract class ContentWidget extends LiveWidget {
-    protected _data: ContentWidgetConfig;
-}
