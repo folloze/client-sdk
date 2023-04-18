dbe951de746e4eeea4623ecc68727975ab203482
Dana Turjeman <dana@folloze.com>
2023-04-18 14:56:51 +0300

    add TrackedLeadDownloadFile to lead event tracking

--- dist/src/common/helpers/leadEventTracking.d.ts
+++ dist/src/common/helpers/leadEventTracking.d.ts
@@ -1,6 +1,7 @@
 import { LitElement } from "lit";
 import { TargetType } from "../../liveboard/ILiveboardTypes";
-declare type TrackedLeadActions = "lead-link-click";
+import { SourceType } from "../../analytics/Analytics";
+declare type TrackedLeadActions = "lead-link-click" | "track-download-file";
 export declare class AbstractLeadTracker {
     payload: unknown;
     action: TrackedLeadActions;
@@ -16,6 +17,13 @@ export declare type TrackedLeadLinkClickPayload = {
 export declare class TrackedLeadLinkClick extends AbstractLeadTracker {
     constructor(payload: TrackedLeadLinkClickPayload);
 }
-export declare type TrackedLeadEvent = TrackedLeadLinkClick;
+export declare class TrackedLeadDownloadFile extends AbstractLeadTracker {
+    constructor(payload: {
+        sourceType: SourceType;
+        contentItemId: number;
+        itemId?: number;
+    });
+}
+export declare type TrackedLeadEvent = TrackedLeadLinkClick | TrackedLeadDownloadFile;
 export declare function trackLeadEvent(el: LitElement, trackedLeadEvent: TrackedLeadEvent, onSuccess?: Function, onError?: Function): void;
 export {};
