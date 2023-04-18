91ab54f0f09a433c9d83d88345f3a325e2ccabb7
Dana Turjeman <dana@folloze.com>
2023-04-18 13:22:19 +0300

    new dist

---
+++ dist/src/common/interfaces/IItemViewer.d.ts
@@ -0,0 +1,13 @@
+import { LitElement } from "lit";
+import { OpenItemViewerPayload } from "../../liveboard/ILiveboardTypes";
+import { LiveWidget } from "../LiveWidget";
+import { FlzEvent } from "../FlzEvent";
+export interface IItemViewer extends LitElement {
+    open(item?: OpenItemViewerPayload): void;
+    close(): void;
+    changeItem(item: OpenItemViewerPayload): any;
+    stateChanged(state: any): any;
+    incomingEvents(e: FlzEvent): any;
+    setItemViewerTopPosition(item?: OpenItemViewerPayload): any;
+    headerLoadedPromise: Promise<LiveWidget>;
+}
