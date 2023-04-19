7c8cce2c51ab28d74fd11f46ffc0bb995ce7d2ad
Dana Turjeman <dana@folloze.com>
2023-04-19 11:08:13 +0300

    update dist

--- dist/analytics/Analytics.js
+++ dist/analytics/Analytics.js
@@ -1,1 +1,1 @@
-import{a,b,c}from"../chunks/chunk.5PWEONAQ.js";import"../chunks/chunk.KVVA2TM3.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
+import{a,b,c}from"../chunks/chunk.DWZYBKC3.js";import"../chunks/chunk.KVVA2TM3.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
