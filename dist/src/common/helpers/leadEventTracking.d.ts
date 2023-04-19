6ab618f5783b15f16ae412605b095cba77c7e604
Dana Turjeman <dana@folloze.com>
2023-04-19 17:01:03 +0300

    new dist files

---
+++ dist/src/common/helpers/leadEventTracking.d.ts
@@ -0,0 +1,36 @@
+import { LitElement } from "lit";
+import { TargetType } from "../../liveboard/ILiveboardTypes";
+import { SourceType } from "../../analytics/Analytics";
+declare type TrackedLeadActions = "lead-link-click" | "track-download-file" | "track-lead-like-content";
+export declare class AbstractLeadTracker {
+    payload: unknown;
+    action: TrackedLeadActions;
+}
+export declare type TrackedLeadLinkClickPayload = {
+    url: string;
+    targetType: TargetType;
+    details: {
+        sectionName: string;
+        widgetTag: string;
+    };
+};
+export declare class TrackedLeadLinkClick extends AbstractLeadTracker {
+    constructor(payload: TrackedLeadLinkClickPayload);
+}
+export declare class TrackedLeadDownloadFile extends AbstractLeadTracker {
+    constructor(payload: {
+        sourceType: SourceType;
+        contentItemId: number;
+        itemId?: number;
+    });
+}
+export declare class TrackedLeadLikeContent extends AbstractLeadTracker {
+    constructor(payload: {
+        sourceType: SourceType;
+        contentItemId: number;
+        itemId?: number;
+    });
+}
+export declare type TrackedLeadEvent = TrackedLeadLinkClick | TrackedLeadDownloadFile;
+export declare function trackLeadEvent(el: LitElement, trackedLeadEvent: TrackedLeadEvent, onSuccess?: Function, onError?: Function): void;
+export {};
