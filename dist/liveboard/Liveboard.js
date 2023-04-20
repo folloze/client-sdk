f530d39e0e7b07ce38f6cff28a58daac0c4e7994
Dana Turjeman <dana@folloze.com>
2023-04-20 09:42:48 +0300

    new dist

---
+++ dist/liveboard/Liveboard.js
@@ -0,0 +1,1 @@
+import{a}from"../chunks/chunk.WXXVDNOQ.js";import"../chunks/chunk.TGN3ZZKD.js";import"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.KVVA2TM3.js";export{a as Liveboard};
---
+++ dist/src/common/interfaces/oldBoardTypes/IOldSideBySide.d.ts
@@ -0,0 +1,57 @@
+import { Cta } from "./IOldBanner";
+export interface IOldSideBySide {
+    show: boolean;
+    areas: Area[];
+}
+export interface Area {
+    key: number;
+    show: boolean;
+    title: Title;
+    strips: Strip[];
+    position: Position;
+}
+export interface Strip {
+    key: number;
+    sections: Section[];
+}
+export interface Section {
+    key: number;
+    data: Data;
+    type: string;
+    position: string;
+}
+export interface Title {
+    text: string;
+    subtext: string;
+}
+export interface Link {
+    url: string;
+    open_type: string;
+}
+export interface Settings {
+    id: number;
+    fit: string;
+    url: string;
+    tint: string;
+    is_loading: boolean;
+    source_type: number;
+}
+export interface Image {
+    link?: Link;
+    settings?: Settings;
+}
+export interface Data {
+    image?: Image;
+    paragraph?: Paragraph;
+}
+export interface Paragraph {
+    cta: Cta;
+    icons: any;
+    title: string;
+    description: string;
+}
+export interface Position {
+    label: string;
+    value: number;
+    location: string;
+}
