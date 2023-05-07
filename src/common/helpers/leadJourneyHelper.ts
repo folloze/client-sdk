8c8353a6badc0cc828055483479c44c883c902b1
Dana Turjeman <dana@folloze.com>
2023-05-07 12:29:23 +0300

    remove method isAiJourney

--- src/common/helpers/leadJourneyHelper.ts
+++ src/common/helpers/leadJourneyHelper.ts
@@ -1,9 +1,5 @@
 import {OpenItemViewerPayload} from "../../liveboard/ILiveboardTypes";
 
-export function isAiJourney(item: OpenItemViewerPayload) {
-   return getJourneyType(item) === "ai";
-}
-
 export function getJourneyType(item: OpenItemViewerPayload) {
    return item.viewer_settings?.lead_journey?.type === "ai" ? "ai" : "item";
 }
