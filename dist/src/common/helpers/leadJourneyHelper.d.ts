630ccc8e1bb02af1bb196d7fcf6f059864a28294
Dana Turjeman <dana@folloze.com>
2023-05-15 16:00:21 +0300

    remove dist

--- dist/src/common/helpers/leadJourneyHelper.d.ts
+++
@@ -1,3 +0,0 @@
-import { OpenItemViewerPayload } from "../../liveboard/ILiveboardTypes";
-export declare function getItemSourceType(item: OpenItemViewerPayload): "item" | "ai";
-export declare function getJourneyType(item: OpenItemViewerPayload): "item" | "ai";
