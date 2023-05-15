848a7294b10545c660106dd2d00e8961253896d0
Dana Turjeman <dana@folloze.com>
2023-05-15 15:16:49 +0300

    add new analyticshelper

--- dist/src/common/helpers/leadJourneyHelper.d.ts
+++ dist/src/common/helpers/leadJourneyHelper.d.ts
@@ -1,3 +1,2 @@
 import { OpenItemViewerPayload } from "../../liveboard/ILiveboardTypes";
-export declare function getItemSourceType(item: OpenItemViewerPayload): "item" | "ai";
 export declare function getJourneyType(item: OpenItemViewerPayload): "item" | "ai";
