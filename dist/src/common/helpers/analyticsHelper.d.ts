95bfc12b26b89a9f26e94c295c909ff7391d2aa2
Dana Turjeman <dana@folloze.com>
2023-05-15 15:40:37 +0300

    change to plural to align it to FE

--- dist/src/common/helpers/analyticsHelper.d.ts
+++ dist/src/common/helpers/analyticsHelper.d.ts
@@ -1,3 +1,3 @@
 import { OpenItemViewerPayload } from "../../liveboard/ILiveboardTypes";
-export declare function getAnalyticsSourceType(item: OpenItemViewerPayload): "item" | "recommendation";
+export declare function getAnalyticsSourceType(item: OpenItemViewerPayload): "recommendations" | "item";
 export declare function getAnalyticsItemId(item: OpenItemViewerPayload): number;
--- src/common/helpers/analyticsHelper.ts
+++ src/common/helpers/analyticsHelper.ts
@@ -1,7 +1,7 @@
 import {OpenItemViewerPayload} from "../../liveboard/ILiveboardTypes";
 
 export function getAnalyticsSourceType(item: OpenItemViewerPayload) {
-   return item.is_content_item === true ? "recommendation" : "item";
+   return item.is_content_item === true ? "recommendations" : "item";
 }
 
 export function getAnalyticsItemId(item: OpenItemViewerPayload) {
