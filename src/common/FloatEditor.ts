e3971a68608570a479444221ae0c58a619532813
ItzhakTzoref07 <itzhak.tzoref@folloze.com>
2023-07-31 16:12:58 +0300

    fix info icon

--- src/common/FloatEditor.ts
+++ src/common/FloatEditor.ts
@@ -181,31 +181,33 @@ export class FloatEditor extends FloatingElement {
                 @mouseover="${this.highlight}"
                 @mouseleave="${this.removeHighlight}">
                 <span class="conf-name"> ${this.title || this.childEl.widget?.widgetTitle || ""} </span>
-                ${this.childEl.widget?.documentationUrl ?
-                    html`<div class="documentation-url" @click=${this.goToDocumentation}>
+                <div class="floating-editor-actions">
+                    ${this.childEl.widget?.documentationUrl ?
+                        html`
+                            <div class="documentation-url" @click=${this.goToDocumentation}>
+                                <svg
+                                    width="24px"
+                                    height="24px"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.25 5.96118H9.75V7.46118H8.25V5.96118ZM8.25 8.96118H9.75V13.4612H8.25V8.96118ZM9 2.21118C4.86 2.21118 1.5 5.57118 1.5 9.71118C1.5 13.8512 4.86 17.2112 9 17.2112C13.14 17.2112 16.5 13.8512 16.5 9.71118C16.5 5.57118 13.14 2.21118 9 2.21118ZM9 15.7112C5.6925 15.7112 3 13.0187 3 9.71118C3 6.40368 5.6925 3.71118 9 3.71118C12.3075 3.71118 15 6.40368 15 9.71118C15 13.0187 12.3075 15.7112 9 15.7112Z"
+                                    fill="currentColor" />
+                                </svg>
+                            </div>` : ""
+                    }
+                    <div class="close" @click=${this.close}>
                         <svg
-                            width="24"
-                            height="24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
                             viewBox="0 0 24 24"
-                            fill="#ffffff"
-                            xmlns="http://www.w3.org/2000/svg">
+                            width="24px"
+                            fill="#ffffff">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
                             <path
-                                d="M8.25 5.96118H9.75V7.46118H8.25V5.96118ZM8.25 8.96118H9.75V13.4612H8.25V8.96118ZM9 2.21118C4.86 2.21118 1.5 5.57118 1.5 9.71118C1.5 13.8512 4.86 17.2112 9 17.2112C13.14 17.2112 16.5 13.8512 16.5 9.71118C16.5 5.57118 13.14 2.21118 9 2.21118ZM9 15.7112C5.6925 15.7112 3 13.0187 3 9.71118C3 6.40368 5.6925 3.71118 9 3.71118C12.3075 3.71118 15 6.40368 15 9.71118C15 13.0187 12.3075 15.7112 9 15.7112Z"
-                                fill="none" />
+                                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
                         </svg>
-                    </div>` : ""
-                }
-                <div class="close" @click=${this.close}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        fill="#ffffff">
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path
-                            d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
-                    </svg>
+                    </div>
                 </div>
             </div>
             <div id="body"></div>
