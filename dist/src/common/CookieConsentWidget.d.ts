0c4d3229ca307113b8862d0982a5df3d11cc0f84
Tanya Laushkin <tanya@folloze.com>
2023-11-02 18:05:10 +0200

    add isExplicitConsent to cookie consent widget

--- dist/src/common/CookieConsentWidget.d.ts
+++ dist/src/common/CookieConsentWidget.d.ts
@@ -2,5 +2,6 @@ import { LiveWidget } from "./LiveWidget";
 export declare abstract class CookieConsentWidget extends LiveWidget {
     isCookieConsentable: boolean;
     abstract getOnlyRegulatedCountriesValue(): any;
+    abstract isExplicitConsent(isRegulatedCountry: boolean): boolean;
     abstract show(): any;
 }
