c70f3c6def77d07dace93193fe8aa5d39aa17c03
Tanya Laushkin <tanya@folloze.com>
2023-11-09 13:30:34 +0200

    delete dist to try to fix build in github

--- dist/analytics/Analytics.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c}from"../chunks/chunk.D33KHSEJ.js";import"../chunks/chunk.KVVA2TM3.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
--- dist/designer/Designer.js
+++
@@ -1,1 +0,0 @@
-import{a as b}from"../chunks/chunk.T4JXVM2I.js";import"../chunks/chunk.OI2NJWH6.js";import{a}from"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.KVVA2TM3.js";export{a as CampaignElementsTypes,b as Designer};
