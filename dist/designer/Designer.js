411cb250155afeefe5064dcefed2970cf7bbd3aa
Tanya Laushkin <tanya@folloze.com>
2023-12-03 11:37:56 +0200

    rename genai endpoint

--- dist/designer/Designer.js
+++ dist/designer/Designer.js
@@ -1,1 +1,1 @@
-import{a as b}from"../chunks/chunk.I4XCWZIH.js";import"../chunks/chunk.OI2NJWH6.js";import{a}from"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.KVVA2TM3.js";export{a as CampaignElementsTypes,b as Designer};
+import{a as b}from"../chunks/chunk.Z75T3BPY.js";import"../chunks/chunk.OI2NJWH6.js";import{a}from"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.KVVA2TM3.js";export{a as CampaignElementsTypes,b as Designer};
