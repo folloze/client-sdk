42c54487d27d05fb61aeacb341913bb92931e330
Adi Heffer <adi.heffer@folloze.com>
2023-12-12 11:43:05 +0200

    FLZ-8326: Add actual edited component to component editor

--- src/common/LiveWidgetComponentEdit.ts
+++ src/common/LiveWidgetComponentEdit.ts
@@ -4,6 +4,7 @@ import get from "lodash/get";
 export abstract class LiveWidgetComponentEdit extends LiveWidgetEdit {
 
     protected _propPath: string;
+    protected _editableComponent?: HTMLElement;
 
     set propertyPath(path: string) {
         this._propPath = path;
@@ -12,6 +13,15 @@ export abstract class LiveWidgetComponentEdit extends LiveWidgetEdit {
     get propertyPath(): string {
         return this._propPath;
     }
+
+    set editableComponent(value: HTMLElement) {
+        this._editableComponent = value;
+    }
+
+    get editableComponent(): HTMLElement {
+        return this._editableComponent;
+    }
+
 
     protected firstUpdated() {
         this.data = get(this.widget.data, this.propertyPath);
