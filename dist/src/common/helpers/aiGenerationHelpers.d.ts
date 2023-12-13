a881ddfc51757514cc0081a4356285d27ad54caa
Tanya Laushkin <tanya@folloze.com>
2023-12-13 12:06:33 +0200

    fix type strict

--- dist/src/common/helpers/aiGenerationHelpers.d.ts
+++ dist/src/common/helpers/aiGenerationHelpers.d.ts
@@ -1,2 +1,2 @@
 import { type GenerationConfig } from "../interfaces/IBoard";
-export declare function canWidgetGenerateText(widgetId: string, widgetTagName: string, generateConfig: GenerationConfig): boolean;
+export declare function canWidgetGenerateText(widgetId: string, widgetTagName: string, generateConfig: GenerationConfig | undefined): boolean;
