70060a471efd42fcd9d3329761b64da29d1094dd
Elad <elad.esterkind@folloze.com>
2024-01-22 10:59:38 +0200

    FLZ-8474 upgraded axios and ran build

--- dist/liveboard/Liveboard.js
+++ dist/liveboard/Liveboard.js
@@ -1,1 +1,1 @@
-import{a}from"../chunks/chunk.SNGR6WWS.js";import"../chunks/chunk.223M7VLT.js";import"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.KVVA2TM3.js";export{a as Liveboard};
+import{a}from"../chunks/chunk.33XSATAX.js";import"../chunks/chunk.J63CCCOP.js";import"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.R2MBWYP6.js";export{a as Liveboard};
