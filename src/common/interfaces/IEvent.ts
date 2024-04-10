5a426dab9eb06c0680b2409904f0e905fd0f1e24
Adi Heffer <adi.heffer@folloze.com>
2024-04-10 16:12:24 +0300

    FLZ-8234: Move beforeunload out of public events

--- src/common/interfaces/IEvent.ts
+++ src/common/interfaces/IEvent.ts
@@ -7,8 +7,7 @@ export type PUBLIC_OUTGOING_EVENT_ACTION =
 export type PUBLIC_INCOMING_EVENT_ACTION =
     | "Folloze.consentGiven"
     | "Folloze.stopTrackingForVisit"
-    | "Folloze.triggerCtaSubmit"
-    | "beforeunload";
+    | "Folloze.triggerCtaSubmit";
 
 export type FLZ_EVENT_ACTION = FLZ_LIVEBOARD_EVENT_ACTION | FLZ_DESIGNER_EVENT_ACTION;
 
