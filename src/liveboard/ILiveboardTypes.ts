3b5d42f7b4b1236ef069746aea8570a89461f733
Elad <elad.esterkind@folloze.com>
2024-09-18 13:22:10 +0300

    FLZ-9907:we don't show category in seo

--- src/liveboard/ILiveboardTypes.ts
+++ src/liveboard/ILiveboardTypes.ts
@@ -34,7 +34,6 @@ export type CategoryResponseV2 = {
     id: number;
     slug: string;
     name: string;
-    seo_title: string;
     category_type: number;
     parent_category_id: number;
     board_id: number;
