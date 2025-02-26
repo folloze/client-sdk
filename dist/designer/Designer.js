bc0ca44f5a7764fbd49cc283435a0938f25d51f2
Tanya Laushkin <tanya@folloze.com>
2025-02-26 19:13:14 +0200

    changity

--- dist/designer/Designer.js
+++ dist/designer/Designer.js
@@ -1,1 +1,1 @@
-import{a as b}from"../chunks/chunk.KMTW3I3T.js";import"../chunks/chunk.HBA5WTQJ.js";import{a}from"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{a as CampaignElementsTypes,b as Designer};
+import{a as b}from"../chunks/chunk.QS72G6OC.js";import"../chunks/chunk.HBA5WTQJ.js";import{a}from"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{a as CampaignElementsTypes,b as Designer};
