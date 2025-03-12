58276b5b926712b8864853e720267e2de2fb3ca7
Tanya Laushkin <tanya@folloze.com>
2025-03-12 15:09:44 +0200

    rebuild

--- dist/chunks/chunk.AZMLWZN7.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/chunks/chunk.UV4YAITP.js
-import{b as c,e as t}from"./chunk.SESKNCL3.js";import{a as n}from"./chunk.G766KHNG.js";import{a}from"./chunk.4KLAD3LI.js";var r=class{constructor(){}static async create(o){let e=new r,i=await c.create(o);return e.fetcher=i,e.analytics=new t(i),e.designer=new n(i),e.liveboard=new a(i),e}};export{r as a};
-========
-import{b as c,e as t}from"./chunk.EIAHBX7U.js";import{a as n}from"./chunk.L54CCHN7.js";import{a}from"./chunk.5UFWVMHW.js";var r=class{constructor(){}static async create(o){let e=new r,i=await c.create(o);return e.fetcher=i,e.analytics=new t(i),e.designer=new n(i),e.liveboard=new a(i),e}};export{r as a};
->>>>>>>> master:dist/chunks/chunk.AZMLWZN7.js
--- dist/designer/Designer.js
+++ dist/designer/Designer.js
@@ -1,1 +1,1 @@
-import{a as b}from"../chunks/chunk.L54CCHN7.js";import"../chunks/chunk.IPSOLFWP.js";import{a}from"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.NHILRETX.js";import"../chunks/chunk.R2MBWYP6.js";export{a as CampaignElementsTypes,b as Designer};
+import{a as b}from"../chunks/chunk.FD5G43OW.js";import"../chunks/chunk.HBA5WTQJ.js";import{a}from"../chunks/chunk.TWUQ6LLZ.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{a as CampaignElementsTypes,b as Designer};
