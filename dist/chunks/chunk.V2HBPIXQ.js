0e357717dcdcc4bd8bce916643726ea231f7ea4d
Tanya Laushkin <tanya@folloze.com>
2025-03-20 14:47:03 +0200

    ttt

--- dist/chunks/chunk.V2HBPIXQ.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/chunks/chunk.V2HBPIXQ.js
-import{b as c,e as t}from"./chunk.SESKNCL3.js";import{a as n}from"./chunk.P6TTYYKZ.js";import{a}from"./chunk.4KLAD3LI.js";var r=class{constructor(){}static async create(o){let e=new r,i=await c.create(o);return e.fetcher=i,e.analytics=new t(i),e.designer=new n(i),e.liveboard=new a(i),e}};export{r as a};
-========
-import{b as c,e as t}from"./chunk.SESKNCL3.js";import{a as n}from"./chunk.NDKTKFKR.js";import{a}from"./chunk.4KLAD3LI.js";var r=class{constructor(){}static async create(o){let e=new r,i=await c.create(o);return e.fetcher=i,e.analytics=new t(i),e.designer=new n(i),e.liveboard=new a(i),e}};export{r as a};
->>>>>>>> master:dist/chunks/chunk.Q46TW5IF.js
