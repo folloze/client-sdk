dacb6784075b208dc8ad307d21ba8cbd1bcda03c
ItzhakTzoref07 <itzhak.tzoref@folloze.com>
2025-04-20 13:13:04 +0300

    FLZ-10457 - merge fixes

--- dist/analytics/Analytics.js
+++ dist/analytics/Analytics.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-import{c as a,d as b,e as c}from"../chunks/chunk.LIOAF37I.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
-=======
-import{c as a,d as b,e as c}from"../chunks/chunk.UTRD5Y7P.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
->>>>>>> FLZ-10457_Item-Settings-Enhancements-2
+import{c as a,d as b,e as c}from"../chunks/chunk.P4LTVLXM.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
