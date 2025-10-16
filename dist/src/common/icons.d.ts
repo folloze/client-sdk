c0aff8258d8f39419ca0fcb5aa42363f8eef50ee
Onur Bozkurt <onurbozkurt@users.noreply.github.com>
2025-10-16 09:57:03 +0300

    FLZ-10980: TileLabel & TileAction types for ItemResponseV2 (#219)

--- dist/src/common/icons.d.ts
+++ dist/src/common/icons.d.ts
@@ -1,2 +1,2 @@
-export declare const infoIcon: import("lit").TemplateResult<1>;
-export declare const closeIcon: import("lit").TemplateResult<1>;
+export declare const infoIcon: import("lit-html").TemplateResult<1>;
+export declare const closeIcon: import("lit-html").TemplateResult<1>;
