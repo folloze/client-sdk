81a36d87eac5923ac853ea094a4e8d22e00e7fcf
Tanya Laushkin <tanya@folloze.com>
2026-01-25 13:45:09 +0200

    try

--- dist/chunks/chunk.EW4EGS3M.js
+++
@@ -1,1 +0,0 @@
-import{b as c,e as t}from"./chunk.VHIKGA5G.js";import{a as n}from"./chunk.YH2JH66L.js";import{a}from"./chunk.GJPB46OD.js";var r=class{constructor(){}static async create(o){let e=new r,i=await c.create(o);return e.fetcher=i,e.analytics=new t(i),e.designer=new n(i),e.liveboard=new a(i),e}};export{r as a};
