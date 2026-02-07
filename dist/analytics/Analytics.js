2c78b339d485ab47ed7721b772febeaba5c1f352
Tal Gruper <tal.gruper@folloze.com>
2026-02-07 20:21:15 +0200

    FLZ-11594 - Updated build

--- dist/analytics/Analytics.js
+++ dist/analytics/Analytics.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-import{d as a,e as b,f as c}from"../chunks/chunk.NLEBNKOJ.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
-||||||| c4924b08
-import{c as a,d as b,e as c}from"../chunks/chunk.LNSXY6X4.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{c as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes};
-=======
-import{c as a,d as b,e as c,f as d}from"../chunks/chunk.C5FCUERS.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{d as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes,c as WidgetEventTypes};
->>>>>>> master
+import{d as a,e as b,f as c,g as d}from"../chunks/chunk.VZ2MOG7J.js";import"../chunks/chunk.ZTUE7IM2.js";import"../chunks/chunk.R2MBWYP6.js";export{d as Analytics,b as DesignerEventTypes,a as LiveBoardEventTypes,c as WidgetEventTypes};
--- dist/sdk.js
+++ dist/sdk.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-import{a}from"./chunks/chunk.72X33SU5.js";import"./chunks/chunk.NLEBNKOJ.js";import"./chunks/chunk.RF7T6BW5.js";import"./chunks/chunk.GJPB46OD.js";import"./chunks/chunk.HBA5WTQJ.js";import"./chunks/chunk.TWUQ6LLZ.js";import"./chunks/chunk.ZTUE7IM2.js";import"./chunks/chunk.R2MBWYP6.js";export{a as ClientSDK};
-||||||| c4924b08
-import{a}from"./chunks/chunk.KRPSOOK6.js";import"./chunks/chunk.LNSXY6X4.js";import"./chunks/chunk.YH2JH66L.js";import"./chunks/chunk.GJPB46OD.js";import"./chunks/chunk.HBA5WTQJ.js";import"./chunks/chunk.TWUQ6LLZ.js";import"./chunks/chunk.ZTUE7IM2.js";import"./chunks/chunk.R2MBWYP6.js";export{a as ClientSDK};
-=======
-import{a}from"./chunks/chunk.Y6ICTQHY.js";import"./chunks/chunk.C5FCUERS.js";import"./chunks/chunk.YH2JH66L.js";import"./chunks/chunk.GJPB46OD.js";import"./chunks/chunk.HBA5WTQJ.js";import"./chunks/chunk.TWUQ6LLZ.js";import"./chunks/chunk.ZTUE7IM2.js";import"./chunks/chunk.R2MBWYP6.js";export{a as ClientSDK};
->>>>>>> master
+import{a}from"./chunks/chunk.73SJBA3X.js";import"./chunks/chunk.VZ2MOG7J.js";import"./chunks/chunk.RF7T6BW5.js";import"./chunks/chunk.GJPB46OD.js";import"./chunks/chunk.HBA5WTQJ.js";import"./chunks/chunk.TWUQ6LLZ.js";import"./chunks/chunk.ZTUE7IM2.js";import"./chunks/chunk.R2MBWYP6.js";export{a as ClientSDK};
