2217104fcd9f57cc8840d600e738c777a6063d52
Tanya Laushkin <tanya@folloze.com>
2026-02-16 12:01:35 +0200

    add identityServiceEndpoint

--- src/common/FetchService.ts
+++ src/common/FetchService.ts
@@ -16,6 +16,7 @@ export type FetcherOptions = {
     csrfToken?: string;
     pingEndpoint?: string;
     analyticsServiceEndpoint: string;
+    identityServiceEndpoint: string;
     flzClientFeature?: "embedded";
 };
 
@@ -35,6 +36,7 @@ const defaultFetcherOptions: FetcherOptions = {
         headers: {},
     },
     analyticsServiceEndpoint: "",
+    identityServiceEndpoint: "",
 };
 
 export const FLZ_SESSION_GUID_HEADER = "folloze-session-guid";
