7d7ab42ddccd6dd9169324fd764363a403f1ac50
Tanya Laushkin <tanya@folloze.com>
2026-03-11 13:39:04 +0200

    add identity service endpoints to client-sdk

---
+++ dist/src/identity/Identity.d.ts
@@ -0,0 +1,15 @@
+import { type FetchService } from "../common/FetchService";
+export type SignInResponse = {
+    session: string;
+    flz_tryme_user?: string;
+};
+export type VerifyEmailResponse = {
+    id: number;
+    email: string;
+};
+export declare class Identity {
+    private fetchService;
+    constructor(fetch: FetchService);
+    signIn(email: string): Promise<SignInResponse>;
+    verifyEmail(email: string, code: string, session: string): Promise<VerifyEmailResponse>;
+}
---
+++ src/identity/Identity.ts
@@ -0,0 +1,52 @@
+import {type FetchService} from "../common/FetchService";
+
+export type SignInResponse = {
+    session: string;
+    flz_tryme_user?: string;
+};
+
+export type VerifyEmailResponse = {
+    id: number;
+    email: string;
+};
+
+export class Identity {
+    private fetchService: FetchService;
+
+    constructor(fetch: FetchService) {
+        this.fetchService = fetch;
+
+        this.fetchService.fetcher.interceptors.request.use(config => {
+            config.withCredentials = true;
+            return config;
+        });
+    }
+
+    public signIn(email: string): Promise<SignInResponse> {
+        return new Promise((resolve, reject) => {
+            this.fetchService.fetcher
+                .post<SignInResponse>(`${this.fetchService.options.identityServiceEndpoint}/tryme/sign_in`, {email})
+                .then(result => {
+                    resolve(result.data);
+                })
+                .catch(e => {
+                    console.error("could not sign in", e);
+                    reject(e);
+                });
+        });
+    }
+
+    public verifyEmail(email: string, code: string, session: string): Promise<VerifyEmailResponse> {
+        return new Promise((resolve, reject) => {
+            this.fetchService.fetcher
+                .post<VerifyEmailResponse>(`${this.fetchService.options.identityServiceEndpoint}/tryme/verify_email`, {email, code, session})
+                .then(result => {
+                    resolve(result.data);
+                })
+                .catch(e => {
+                    console.error("could not verify email", e);
+                    reject(e);
+                });
+        });
+    }
+}
